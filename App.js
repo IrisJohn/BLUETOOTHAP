1635f353943b83e851988057a531a8ee7160999d
IrisJohn <43463438+IrisJohn@users.noreply.github.com>
2021-01-23 19:51:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import video from './src/video/video'
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+im
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+
+<video>
+  
+</video>    
+  );
+};
+
+
+
+export default App;
